2a635e04435789f4323b1a0160d444b2cf749176
fernanda <ferformigas@gmail.com>
2021-01-18 11:52:41 -0300

    Minor fixes

--- js/script.js
+++ js/script.js
@@ -39,3 +39,4 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
